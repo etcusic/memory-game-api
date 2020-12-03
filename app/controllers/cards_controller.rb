226f6a22ac20057c6ff88ea28c2ed8b1968c48a6
Eli Cusic <tntitans31@gmail.com>
2020-12-03 13:42:48 -0600

    Added routes and controllers for cards and decks

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,9 @@
+class CardsController < ApplicationController
+    def index
+
+    end
+
+    def show
+        
+    end
+end
---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,9 @@
+class DecksController < ApplicationController
+    def index
+
+    end
+
+    def show
+        
+    end
+end
