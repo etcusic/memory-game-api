54ea1939417c943947c63b2179d56428d63dbfe3
Eli Cusic <tntitans31@gmail.com>
2021-01-15 19:18:40 -0600

    built out ConnectFourLogsController

--- app/controllers/connect_four_logs_controller.rb
+++ app/controllers/connect_four_logs_controller.rb
@@ -1,2 +1,20 @@
 class ConnectFourLogsController < ApplicationController
+       
+    def index
+        @connect_four_logs = ConnectFourLog.order('score DESC')[0..11].map { | log | {user: log.user.name, deck: log.deck.title, time: log.time, outcome: log.outcome} }
+        render json: @connect_four_logs
+    end
+
+    def create
+        @connect_four_log = ConnectFourLog.new(game_log_params)
+        @connect_four_log.save
+        redirect_to '/decks' 
+    end
+
+    private
+
+    def game_log_params
+        params.require(:connect_four_log).permit(:user_id, :deck_id, :time, :outcome)
+    end
+
 end
